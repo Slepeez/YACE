83c76104fda0e7e590aedf2c851309694a2706f3
Slepeez <barbalhoa78@gmail.com>
2026-09-23 23:10:32 -0300

    feature: implement chip8_emulate_cycle function and fix chip8_init function

--- src/chip8.c
+++ src/chip8.c
@@ -1,5 +1,16 @@
 #include "chip8.h"
 
-void chip8_init(Chip8* chip){
-    
+void chip8_init(Chip8 *chip) { 
+    memset(&chip, 0, sizeof(Chip8)); 
+    chip->pc = START_ADDRESS;
+}
+void chip8_load_program(Chip8 *chip, const uint8_t *program, size_t size) {
+    if(size > MEMORY_SIZE - START_ADDRESS) {
+        // Handle error: program too large to fit in memory
+        return;
+    }
+    memcpy(&chip->memory[START_ADDRESS], program, size);
+}
+void chip8_emulate_cycle(Chip8 * chip){
+    uint8_t opcode = chip->memory
 }
